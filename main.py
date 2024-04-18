b29c11a36bbeeba70178d8132e273c365631c3c5
WiktoriaB08 <166690497+WiktoriaB08@users.noreply.github.com>
2024-04-18 08:44:13 +0200

    wp: add user printing funkcionality

---
+++ main.py
@@ -0,0 +1,19 @@
+imie="Dawid"
+nazwisko="Bałuka"
+liczba_postów=6000
+
+print(f"Twój znajomy {imie} {nazwisko} opublikował {liczba_postów} postów")
+
+imie_2="Kewin"
+nazwisko_2="Czajkowski"
+liczba_postów_2=6002
+
+print(f"Twój znajomy {imie_2} {nazwisko_2} opublikował {liczba_postów_2} postów")
+
+imie_3="Kamil"
+nazwisko_3="Gil"
+liczba_postów_3=1_000_000
+
+print(f"Twój znajomy {imie_3} {nazwisko_3} opublikował {liczba_postów_3} postów")
+
+
