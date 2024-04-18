4667ce357a301edbde2099a838e4717281717308
WiktoriaB08 <166690497+WiktoriaB08@users.noreply.github.com>
2024-04-18 10:48:46 +0200

    wip:added a function create new user

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from models.data import users
-from utils.crud import show_users
-
-
-
+from utils.crud import show_users, add_new_user
 
 
 
@@ -11,10 +8,15 @@ if __name__ == "__main__":
     print("Witaj użytkowniku")
     while True:
         print("Menu:")
+        print("0. Zakończ program")
         print("1. Wyświetl co u znajomych")
+        print("2. Dodaj użytkownika")
         menu_option: str = input("dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+           add_new_user(users)
+
