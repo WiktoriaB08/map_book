f558115d6e347d7d809753ba6f8d6eef32fe929e
WiktoriaB08 <166690497+WiktoriaB08@users.noreply.github.com>
2024-04-18 09:02:35 +0200

    wip:prepare for loop

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-imie="Dawid"
-nazwisko="Bałuka"
-liczba_postów=6000
+users: list[dict] = [
+    {"name": "Dawid","surname": "Bałuka", "posts": 6000},
+    {"name": "Kewin","surname": "Czajkowski", "posts": 6002},
+    {"name": "Kamil","surname": "Gil", "posts": 1_000_000},
+    {"name": "Daniel","surname": "Błaszczyk", "posts": 6}
 
-print(f"Twój znajomy {imie} {nazwisko} opublikował {liczba_postów} postów")
+]
 
-imie_2="Kewin"
-nazwisko_2="Czajkowski"
-liczba_postów_2=6002
-
-print(f"Twój znajomy {imie_2} {nazwisko_2} opublikował {liczba_postów_2} postów")
-
-imie_3="Kamil"
-nazwisko_3="Gil"
-liczba_postów_3=1_000_000
-
-print(f"Twój znajomy {imie_3} {nazwisko_3} opublikował {liczba_postów_3} postów")
+for user in users:
+    print(f"twój znajomy {user['name']} opublikował: {user['posts']}")
 
 
